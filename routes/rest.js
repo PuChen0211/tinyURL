d0f5c9a93f394f7270e52b3c47aaf942e70c0aa8
PuChen0211 <pu_chen@tamu.edu>
2016-08-14 00:19:31 -0500

    modified:   routes/redirect.js 	modified:   routes/rest.js 	modified:   server.js 	modified:   services/urlService.js

--- routes/rest.js
+++ routes/rest.js
@@ -6,11 +6,14 @@ var router = express.Router();
 
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var urlService = require('../services/urlService');
 
-router.post('/urls', jsonParser, function (req, res) {
-    var longUrl = req.body.longUrl;
-    // implement this
-    res.json("implement this");
+router.post('/urls', jsonParser, function(req, res) {
+    var shortUrl = urlService.getShortUrl(req.body.longUrl, req.app.longToShortHash, req.app.shortToLongHash);
+    res.json({
+        shortUrl: shortUrl,
+        longUrl: req.body.longUrl
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var app = express();
 var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
 
+app.longToShortHash = {};
+app.shortToLongHash = {};
+
 app.use('/api/v1', restRouter);
 
 app.use('/:shortUrl', redirectRouter); //can cache anything except '/'
